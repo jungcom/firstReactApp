675b76bd27ab71077e982852fbb808275aa15735
Anthony Lee <anthonylee@umass.edu>
2018-12-30 13:14:49 -0500

    Added button function with REact components using state and props

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name : 'Alex', age: 28},
+      {name : 'Bob', age: 24},
+      {name : 'Steve', age: 20}
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        {name : 'Alexious', age: 29},
+        {name : 'Bob', age: 24},
+        {name : 'Steve', age: 20}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I like to ride a bike</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'));
   }
 }
 
