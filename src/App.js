9e3b072b85b2571e36cb9ee6e2eae63f5dff51d5
Anthony Lee <anthonylee@umass.edu>
2019-01-02 12:57:53 -0500

    added input fields that take in arguments and change the textview values.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Person from './Person'
 
 class App extends Component {
+  //state is a given attribute for Component classes. we use this because when the state is changed, the render() call gets called
   state = {
     persons: [
       {name : 'Alex', age: 28},
@@ -11,10 +12,20 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     this.setState({
       persons: [
-        {name : 'Alexious', age: 29},
+        {name : newName, age: 29},
+        {name : 'Bob', age: 24},
+        {name : 'Steve', age: 20}
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) =>{
+    this.setState({
+      persons: [
+        {name : event.target.value, age: 29},
         {name : 'Bob', age: 24},
         {name : 'Steve', age: 20}
       ]
@@ -25,10 +36,18 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I like to ride a bike</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler("Maxmilian")}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, "Max")}
+          changed={this.nameChangedHandler}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}>I like to ride a bike</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'));
--- src/Person.js
+++ src/Person.js
@@ -3,8 +3,9 @@ import React from 'react';
 const person = (props) => {
 	return (
 		<div>
-			<p>I am {props.name} and {props.age} years old!</p>
+			<p onClick={props.click}>I am {props.name} and {props.age} years old!</p>
 			<p>{props.children}</p>
+			<input type='next' onChange={props.changed} value={props.name}></input>
 		</div>
 	)
 };
