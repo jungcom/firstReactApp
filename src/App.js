e42701091e08ba9e688378ae450a9fe4a402a7f0
Anthony Lee <anthonylee@umass.edu>
2019-01-24 15:13:02 -0500

    CSS modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person'
 import './Person/Person.css'
 
@@ -70,18 +70,18 @@ class App extends Component {
       style.backgroundColor = 'red'
     }
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2){
-      classes.push('red')
+      assignedClasses.push(classes.red)
     }
     if (this.state.persons.length <= 1){
-      classes.push('bold')
+      assignedClasses.push(classes.bold)
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi</h1>
-          <p className={classes.join(' ')}>This really works!!</p>
+          <p className={assignedClasses.join(' ')}>This really works!!</p>
           <button style={style} onClick={this.togglepersonHandler}>Switch Name</button>
           {persons}
         </div>
